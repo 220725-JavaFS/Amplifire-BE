f4652c701f9c96040f9aa9a7232f8dc6726938ac
home_laptop <CarlosMaciasCusiter@gmail.com>
2022-09-12 21:12:27 -0700

    modified login to a get request

--- src/main/java/com/revature/Controllers/LoginController.java
+++ src/main/java/com/revature/Controllers/LoginController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +27,7 @@ public class LoginController {
 		this.libraryService = libraryService;
 	}
 
-	@PostMapping
+	@GetMapping
 	public ResponseEntity<String> loginUser(@RequestParam("userName") String userName,
 			@RequestParam("password") String password, HttpSession session) {
 		LibraryUser user = libraryService.loginUser(userName, password);
